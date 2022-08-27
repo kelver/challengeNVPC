9d53f5befd5e844e37a25a92ab24e3220aa0d13e
Kelver <kelver_kmv@hotmail.com>
2022-08-27 00:43:13 -0300

    style: Add dist.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/challengeNVPC.github.io'
+  publicPath: '/challengeNVPC'
 })
